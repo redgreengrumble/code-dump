cb3d5c6e0086c4cce56f12115b43895db4c7edc0
ermanzi <ermanzi@mit.edu>
2017-08-27 20:04:11 -0400

    code dump

--- gen_unique.py
+++ gen_unique.py
@@ -6,11 +6,10 @@ import sys
 
 QUERY_LOG_PATH = sys.argv[1]
 vocab = {}
-unique_set = set()
 
 def gen_unique_queries():
 	global vocab
-	global unique_set
+	unique_set = set()
 	outfile = "unique.%s.%s"%(QUERY_LOG_PATH, datetime.datetime.now().strftime("%d%H%M"))
 	with open(QUERY_LOG_PATH) as f:
 		for line in f:
